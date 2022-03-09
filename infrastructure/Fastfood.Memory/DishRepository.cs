bed2f2e1c870078ddc2ee03f0a03dcf35e8b9b61
Александр <ivashininad@gmail.com>
2022-03-09 14:00:38 +0300

    add web app

--- infrastructure/Fastfood.Memory/DishRepository.cs
+++ infrastructure/Fastfood.Memory/DishRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Fastfood.Memory
 {
-    public class DishRepository
+    public class DishRepository : IDishRepository
     {
+        private readonly Dish[] dishes = new[]
+        {
+            new Dish(1, "Pizza Margarita"),
+            new Dish(2, "Pizza Peperoni"),
+            new Dish(3, "Pizza Three Cheeses"),
+            new Dish(4, "Caesar salad")
+        };
+        public Dish[] GetAllByTitle(string titlePart)
+        {
+            return dishes.Where(dish => dish.Name.Contains(titlePart)).ToArray();
+        }
     }
 }
