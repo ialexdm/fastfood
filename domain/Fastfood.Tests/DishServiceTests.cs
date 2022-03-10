16b3356e20526d3a34463fa82d847064039621ce
Александр <ivashininad@gmail.com>
2022-03-10 18:42:56 +0300

    add to Dish properies description and price

--- domain/Fastfood.Tests/DishServiceTests.cs
+++ domain/Fastfood.Tests/DishServiceTests.cs
@@ -13,12 +13,14 @@ namespace Fastfood.Tests
         [Fact]
         public void GetAllByQuery_WithTtk_CallsGetByTtk()
         {
+            var idOfTtkSearch = 1;
+            var idOfNameOrCategorySearch = 2;
             var dishRepositoryStub = new Mock<IDishRepository>();
             dishRepositoryStub.Setup(x => x.GetByTtk(It.IsAny<string>()))
-                .Returns(new[] { new Dish(1,"", "", "") });
+                .Returns(new[] { new Dish(idOfTtkSearch, "", "", "", "", 1.00m) });
 
             dishRepositoryStub.Setup(x => x.GetAllByNameOrCategory(It.IsAny<string>()))
-                .Returns(new[] { new Dish(2, "", "", "") });
+                .Returns(new[] { new Dish(idOfNameOrCategorySearch, "", "", "", "", 1.00m) });
 
             var dishService = new DishService(dishRepositoryStub.Object);
             var validTtk = "TtK 1234";
@@ -29,12 +31,14 @@ namespace Fastfood.Tests
         [Fact]
         public void GetAllByQuery_WithAuthor_CallsGetAllByNameOrCategory()
         {
+            var idIfTtk = 1;
+            var ifIfNameOrCategory = 2;
             var dishRepositoryStub = new Mock<IDishRepository>();
             dishRepositoryStub.Setup(x => x.GetByTtk(It.IsAny<string>()))
-                .Returns(new[] { new Dish(1, "", "", "") });
+                .Returns(new[] { new Dish(idIfTtk, "", "", "", "", 0m) });
 
             dishRepositoryStub.Setup(x => x.GetAllByNameOrCategory(It.IsAny<string>()))
-                .Returns(new[] { new Dish(2, "", "", "") });
+                .Returns(new[] { new Dish(ifIfNameOrCategory, "", "", "", "", 0m) });
 
             var dishService = new DishService(dishRepositoryStub.Object);
             var invalidTtk = "1234ttk";
--- infrastructure/Fastfood.Memory/DishRepository.cs
+++ infrastructure/Fastfood.Memory/DishRepository.cs
@@ -9,20 +9,26 @@ namespace Fastfood.Memory
         {
             new Dish(1,
                 "Margherita",
-                "Pizza",
-                "TTK 2117"),
+                "Pizza","TTK 2117",
+                "is a typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt, and extra-virgin olive oil.",
+                500.00m),
             new Dish(2,
                 "Pepperoni",
                 "Pizza",
-                "TTK 2115"),
+                "TTK 2115","According to Convenience Store Decisions, Americans consume 251.7 million pounds of pepperoni annually, on 36% of all pizzas produced nationally.",
+                650.00m),
             new Dish(3,
                 "Three Cheeses",
                 "Pizza",
-                "TTK 2281"),
+                "TTK 2281",
+                "is a variety of pizza in Italian cuisine that is topped with a combination of four kinds of cheese, usually melted together, with (rossa, red) or without (bianca, white) tomato sauce. It is popular worldwide, including in Italy,[1] and is one of the iconic items from Pizzerias' menus.",
+                750.00m),
             new Dish(4,
                 "Caesar",
                 "Salad",
-                "TTK 3131")
+                "TTK 3131",
+                "is a green salad of romaine lettuce and croutons dressed with lemon juice (or lime juice), olive oil, egg, Worcestershire sauce, anchovies, garlic, Dijon mustard, Parmesan cheese, and black pepper.",
+                350.00m)
         };
 
         public Dish[] GetAllByNameOrCategory(string query)
